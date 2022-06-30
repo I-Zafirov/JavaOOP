d02e821bfa760f57ed4648484b94ca7d8ce9824c
I-Zafirov <ivoomail@gmail.com>
2022-06-30 13:48:07 +0300

    commit

---
+++ src/T03WorkingWithAbstraction/lab/L02PointInRectangle/Rectangle.java
@@ -0,0 +1,15 @@
+package T03WorkingWithAbstraction.lab.L02PointInRectangle;
+
+public class Rectangle {
+    private Point A;
+    private Point C;
+
+    public Rectangle(Point A, Point C) {
+        this.A = A;
+        this.C = C;
+    }
+
+    public boolean contains(Point x) {
+        return x.greaterOrEqual(A) && x.lessOrEqual(C);
+    }
+}
