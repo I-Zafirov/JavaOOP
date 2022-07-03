17121f8b670849808f06c3fef5f44e1db751047f
I-Zafirov <ivoomail@gmail.com>
2022-07-03 19:49:57 +0300

    commit

---
+++ src/T03WorkingWithAbstraction/exercise/E03CardsWithPower/CardSuits.java
@@ -0,0 +1,18 @@
+package T03WorkingWithAbstraction.exercise.E03CardsWithPower;
+
+public enum CardSuits {
+    CLUBS(0),
+    DIAMONDS(13),
+    HEARTS(26),
+    SPADES(39);
+
+    private int suitPower;
+
+    CardSuits(int suitPower) {
+        this.suitPower = suitPower;
+    }
+
+    public int getSuitsPower() {
+        return suitPower;
+    }
+}
