ba3bfc056b5063177ac935ede05a830c2afe89e5
I-Zafirov <ivoomail@gmail.com>
2022-07-03 19:52:40 +0300

    commit

---
+++ src/T03WorkingWithAbstraction/exercise/E05JediGalaxy/Galaxy.java
@@ -0,0 +1,21 @@
+package T03WorkingWithAbstraction.exercise.E05JediGalaxy;
+
+public class Galaxy {
+    private StarsField field;
+    private Jedi jedi;
+    private Evil evil;
+
+    public Galaxy(StarsField field) {
+        this.field = field;
+        this.jedi = new Jedi();
+        this.evil = new Evil();
+    }
+
+    public void moveEvil(int currentRowEvil, int currentColEvil) {
+      evil.moveEvil(currentRowEvil, currentColEvil, this.field);
+    }
+
+    public long moveJedi(int currentRowJedi, int currentColJedi){
+        return jedi.moveJedi(currentRowJedi, currentColJedi, this.field);
+    }
+}
