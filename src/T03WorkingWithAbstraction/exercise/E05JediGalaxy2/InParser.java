aaa7700b51d7437cf30835a05fd3a66d8ad1fc1a
I-Zafirov <ivoomail@gmail.com>
2022-07-04 07:51:35 +0300

    commit

---
+++ src/T03WorkingWithAbstraction/exercise/E05JediGalaxy2/InParser.java
@@ -0,0 +1,12 @@
+package T03WorkingWithAbstraction.exercise.E05JediGalaxy2;
+
+import java.util.Arrays;
+
+public class InParser {
+    // no instance needed => static
+    public static int[] parseArray (String input, String pattern){
+        return Arrays.stream(input.split(pattern))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+}
