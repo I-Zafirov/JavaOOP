a1b78e2e71ec62e18e49bd8f713ac840314a34d8
I-Zafirov <ivoomail@gmail.com>
2022-07-10 18:07:14 +0300

    commit

---
+++ src/T07Inheritance/lab/L06StackOfStrings/Main.java
@@ -0,0 +1,11 @@
+package T07Inheritance.lab.L06StackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StackOfStrings strings = new StackOfStrings();
+
+        strings.push("my-solution");
+        System.out.println(strings.peek());
+    }
+}
