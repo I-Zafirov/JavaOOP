1cbe20e6eb99ab9f63b24a5d257fd3b27056b0f7
I-Zafirov <ivoomail@gmail.com>
2022-07-11 19:54:14 +0300

    commit

---
+++ src/T07Inheritance/exercise/E02Zoo/Animal.java
@@ -0,0 +1,18 @@
+package T07Inheritance.exercise.E02Zoo;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
