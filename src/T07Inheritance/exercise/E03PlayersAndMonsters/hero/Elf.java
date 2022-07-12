8bb088a9aa6e715dc54588378e8e9e780fc86db5
I-Zafirov <ivoomail@gmail.com>
2022-07-12 13:50:46 +0300

    commit

---
+++ src/T07Inheritance/exercise/E03PlayersAndMonsters/hero/Elf.java
@@ -0,0 +1,8 @@
+package T07Inheritance.exercise.E03PlayersAndMonsters.hero;
+
+public class Elf extends Hero {
+
+    public Elf(String username, int level) {
+        super(username, level);
+    }
+}
