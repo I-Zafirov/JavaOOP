cf654deee6a90f28db27ca685b16bd0df83282f8
I-Zafirov <ivoomail@gmail.com>
2022-07-12 13:50:37 +0300

    commit

---
+++ src/T07Inheritance/exercise/E03PlayersAndMonsters/hero/DarkWizard.java
@@ -0,0 +1,7 @@
+package T07Inheritance.exercise.E03PlayersAndMonsters.hero;
+
+public class DarkWizard extends Wizard {
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
