91fe129308c61b9c99e392c56ae8c77825b8d372
I-Zafirov <ivoomail@gmail.com>
2022-07-12 13:51:26 +0300

    commit

---
+++ src/T07Inheritance/exercise/E03PlayersAndMonsters/hero/MuseElf.java
@@ -0,0 +1,7 @@
+package T07Inheritance.exercise.E03PlayersAndMonsters.hero;
+
+public class MuseElf extends Elf {
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+}
