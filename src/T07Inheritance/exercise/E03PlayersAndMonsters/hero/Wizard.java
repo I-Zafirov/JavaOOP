1da34dc28029757818fdd3ffc4bf492a4ac9a228
I-Zafirov <ivoomail@gmail.com>
2022-07-12 13:51:48 +0300

    commit

---
+++ src/T07Inheritance/exercise/E03PlayersAndMonsters/hero/Wizard.java
@@ -0,0 +1,7 @@
+package T07Inheritance.exercise.E03PlayersAndMonsters.hero;
+
+public class Wizard extends Hero {
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+}
