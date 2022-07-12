9e9de5973665d460a335171d198b40e7d0a823d1
I-Zafirov <ivoomail@gmail.com>
2022-07-12 13:51:37 +0300

    commit

---
+++ src/T07Inheritance/exercise/E03PlayersAndMonsters/hero/SoulMaster.java
@@ -0,0 +1,8 @@
+package T07Inheritance.exercise.E03PlayersAndMonsters.hero;
+
+public class SoulMaster extends DarkWizard {
+
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
