337cdd03386a46455e2a6eb3e1f861b59442bc3c
I-Zafirov <ivoomail@gmail.com>
2022-07-12 13:50:26 +0300

    commit

---
+++ src/T07Inheritance/exercise/E03PlayersAndMonsters/hero/DarkKnight.java
@@ -0,0 +1,7 @@
+package T07Inheritance.exercise.E03PlayersAndMonsters.hero;
+
+public class DarkKnight extends Knight {
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+}
