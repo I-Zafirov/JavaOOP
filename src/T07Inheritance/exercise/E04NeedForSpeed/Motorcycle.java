7a3429ada40448cbb1fa0254c008ce294401102b
I-Zafirov <ivoomail@gmail.com>
2022-07-13 14:12:38 +0300

    commit

---
+++ src/T07Inheritance/exercise/E04NeedForSpeed/Motorcycle.java
@@ -0,0 +1,7 @@
+package T07Inheritance.exercise.E04NeedForSpeed;
+
+public class Motorcycle extends Vehicle{
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
