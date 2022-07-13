63ac34fda028e3b39281d9d50d1210b118b22c03
I-Zafirov <ivoomail@gmail.com>
2022-07-13 14:12:03 +0300

    commit

---
+++ src/T07Inheritance/exercise/E04NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package T07Inheritance.exercise.E04NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle{
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
