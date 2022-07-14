d83a506b19431362ba0fe2027a799b3ecf748292
I-Zafirov <ivoomail@gmail.com>
2022-07-14 08:10:11 +0300

    commit

---
+++ src/T07Inheritance/exercise/E05Restaurant/ColdBeverage.java
@@ -0,0 +1,9 @@
+package T07Inheritance.exercise.E05Restaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage {
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
