2716098ec7f9c917548a5ae91eab27668064448c
I-Zafirov <ivoomail@gmail.com>
2022-07-19 12:20:46 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/lab/L02CarShopExtended/Car.java
@@ -0,0 +1,13 @@
+package T09InterfacesAndAbstraction.lab.L02CarShopExtended;
+
+public interface Car {
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+
+}
