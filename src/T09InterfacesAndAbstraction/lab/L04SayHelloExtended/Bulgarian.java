db3900e1a5b491182825eb213b04e7c1fdb700df
I-Zafirov <ivoomail@gmail.com>
2022-07-20 11:33:43 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/lab/L04SayHelloExtended/Bulgarian.java
@@ -0,0 +1,13 @@
+package T09InterfacesAndAbstraction.lab.L04SayHelloExtended;
+
+public class Bulgarian extends BasePerson {
+
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
