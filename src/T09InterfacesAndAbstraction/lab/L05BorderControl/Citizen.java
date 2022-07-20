1ce4281d7ae80b6a00f547bcd284ff6e3277c221
I-Zafirov <ivoomail@gmail.com>
2022-07-20 11:35:03 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/lab/L05BorderControl/Citizen.java
@@ -0,0 +1,21 @@
+package T09InterfacesAndAbstraction.lab.L05BorderControl;
+
+public class Citizen extends IdentifiableImpl {
+
+    private int age;
+    private String name;
+
+    public Citizen(String id, int age, String name) {
+        super(id);
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
