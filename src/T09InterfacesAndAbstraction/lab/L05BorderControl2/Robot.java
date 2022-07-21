3f173973ad21bb52fe266db29ad9ec9229909f06
I-Zafirov <ivoomail@gmail.com>
2022-07-21 08:59:36 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/lab/L05BorderControl2/Robot.java
@@ -0,0 +1,20 @@
+package T09InterfacesAndAbstraction.lab.L05BorderControl2;
+
+public class Robot implements Identifiable {
+    private String model;
+    private String id;
+
+    public Robot(String model, String id) {
+        this.id = id;
+        this.model = model;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
