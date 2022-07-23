e1a0d340ae65ad92c406d81e132ae865a0b51da0
I-Zafirov <ivoomail@gmail.com>
2022-07-23 09:36:42 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E03BirthdayCelebrations/Pet.java
@@ -0,0 +1,20 @@
+package T09InterfacesAndAbstraction.exercise.E03BirthdayCelebrations;
+
+public class Pet implements Birthable {
+    private String name;
+    private String birthDate;
+
+    public Pet(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
