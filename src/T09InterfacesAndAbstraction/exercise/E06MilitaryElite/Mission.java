09aeabdd216203afb28c48b0c7ddaa6d3c9c1516
I-Zafirov <ivoomail@gmail.com>
2022-07-25 09:24:35 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E06MilitaryElite/Mission.java
@@ -0,0 +1,40 @@
+package T09InterfacesAndAbstraction.exercise.E06MilitaryElite;
+
+import T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Enum.State;
+
+import java.util.Arrays;
+
+public class Mission {
+    private String codeName;
+    private State state;
+
+    public Mission(String codeName, State state) {
+        this.codeName = codeName;
+        setState(state);
+    }
+
+    private void setState(State state) {
+        boolean stateExists = Arrays.asList(State.values()).contains(state);
+        if(!stateExists) {
+            throw new IllegalArgumentException();
+        }
+        this.state = state;
+    }
+
+    public void completeMission() {
+        state = State.finished;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Code Name: %s State: %s", codeName, state.name());
+    }
+}
