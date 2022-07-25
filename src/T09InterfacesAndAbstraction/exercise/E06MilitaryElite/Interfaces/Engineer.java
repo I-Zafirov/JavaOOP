3298e25b78fddec036ccbe9ad28e8e3e9235baf6
I-Zafirov <ivoomail@gmail.com>
2022-07-25 09:22:39 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E06MilitaryElite/Interfaces/Engineer.java
@@ -0,0 +1,9 @@
+package T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Interfaces;
+
+import T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Repair;
+
+import java.util.Collection;
+
+public interface Engineer {
+    Collection<Repair> getRepairs();
+}
