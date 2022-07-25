89d0d9448692d3c802e84388e4737da7ed216cfd
I-Zafirov <ivoomail@gmail.com>
2022-07-25 09:22:48 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E06MilitaryElite/Interfaces/LieutenantGeneral.java
@@ -0,0 +1,7 @@
+package T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Interfaces;
+
+import java.util.List;
+
+public interface LieutenantGeneral {
+    List<Private> getCommandPrivateImpl();
+}
