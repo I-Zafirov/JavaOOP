06c7fc8b4b3dea6679cd67d29b4862311039f4bc
I-Zafirov <ivoomail@gmail.com>
2022-07-25 09:22:28 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E06MilitaryElite/Interfaces/Commando.java
@@ -0,0 +1,9 @@
+package T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Interfaces;
+
+import T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Mission;
+
+import java.util.Collection;
+
+public interface Commando {
+    Collection<Mission> getMissions();
+}
