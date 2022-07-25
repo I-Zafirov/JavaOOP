563e2b901efe82d161980d67020f7c6d432ad1c4
I-Zafirov <ivoomail@gmail.com>
2022-07-25 09:23:15 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E06MilitaryElite/Interfaces/SpecialisedSoldier.java
@@ -0,0 +1,7 @@
+package T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Interfaces;
+
+import T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Enum.Corps;
+
+public interface SpecialisedSoldier {
+    Corps getCorps();
+}
