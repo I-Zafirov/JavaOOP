33705fede8e711d649a28767ef5ea364523bc94a
I-Zafirov <ivoomail@gmail.com>
2022-07-26 11:06:19 +0300

    commit

---
+++ src/T11Polymorphism/lab/L02Shapes/Shape.java
@@ -0,0 +1,11 @@
+package T11Polymorphism.lab.L02Shapes;
+
+public abstract class Shape {
+
+    protected Double perimeter;
+    protected Double area;
+
+    public abstract Double calculatePerimeter();
+
+    public abstract Double calculateArea();
+}
