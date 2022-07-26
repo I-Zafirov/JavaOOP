bf0229a41c88616e5495e4976620b080a187a5d6
I-Zafirov <ivoomail@gmail.com>
2022-07-26 11:03:14 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E06MilitaryElite/Soldiers/LieutenantGeneralImpl.java
@@ -0,0 +1,47 @@
+package T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Soldiers;
+
+import T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Interfaces.LieutenantGeneral;
+import T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Interfaces.Private;
+import T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Interfaces.Soldier;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LieutenantGeneralImpl extends PrivateImpl implements LieutenantGeneral {
+    private List<Private> commandPrivateImpl;
+
+    public LieutenantGeneralImpl(int id, String firstName, String lastName, double salary) {
+        super(id, firstName, lastName, salary);
+        this.commandPrivateImpl = new ArrayList<>();
+    }
+
+    public void addPrivate(Private priv){
+        commandPrivateImpl.add(priv);
+    }
+
+    @Override
+    public List<Private> getCommandPrivateImpl() {
+        return commandPrivateImpl;
+    }
+
+
+
+    @Override
+    public String toString() {
+        //"Name: {firstName} {lastName} Id: {id} Salary: {salary}
+        //Privates:
+        //  {private1 ToString()}
+        //  {private2 ToString()}
+        //  …
+        //  {privateN ToString()}"
+        //Note: privates must be sorted by id in descending order.
+        return super.toString() + System.lineSeparator() +
+                "Privates:" + (this.commandPrivateImpl.isEmpty() ? "" : System.lineSeparator() +
+                commandPrivateImpl.stream().
+                        sorted(Comparator.comparing(Soldier::getId).reversed()).
+                        map(p -> "  " + p).
+                        collect(Collectors.joining(System.lineSeparator())));
+    }
+}
