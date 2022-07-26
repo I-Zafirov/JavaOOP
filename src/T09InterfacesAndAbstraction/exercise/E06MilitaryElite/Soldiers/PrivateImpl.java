4b3c7db41cce99747a891bf84d458853210a0194
I-Zafirov <ivoomail@gmail.com>
2022-07-26 11:03:28 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E06MilitaryElite/Soldiers/PrivateImpl.java
@@ -0,0 +1,23 @@
+package T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Soldiers;
+
+import T09InterfacesAndAbstraction.exercise.E06MilitaryElite.Interfaces.Private;
+
+public class PrivateImpl extends SoldierImpl implements Private {
+    private double salary;
+
+    public PrivateImpl(int id, String firstName, String lastName, double salary) {
+        super(id, firstName, lastName);
+        this.salary = salary;
+    }
+
+    @Override
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        //"Name: {firstName} {lastName} Id: {id} Salary: {salary}"
+        return String.format("Name: %s %s Id: %d Salary: %.2f", getFirstName(), getLastName(), getId(), salary);
+    }
+}
