bf496ab272ceaab09e525ddf338c8df06da3409f
I-Zafirov <ivoomail@gmail.com>
2022-07-26 11:06:09 +0300

    commit

---
+++ src/T11Polymorphism/lab/L02Shapes/Rectangle.java
@@ -0,0 +1,30 @@
+package T11Polymorphism.lab.L02Shapes;
+
+public class Rectangle extends Shape {
+
+    private final Double height;
+    private final Double width;
+
+    public Rectangle(Double height, Double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public Double calculatePerimeter() {
+        if (perimeter == null) {
+            perimeter = 2 * height + 2 * width;
+        }
+
+        return perimeter;
+    }
+
+    @Override
+    public Double calculateArea() {
+        if (area == null) {
+            area = height * width;
+        }
+
+        return area;
+    }
+}
