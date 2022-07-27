5b518114c5a6cf5e4251f93383071bcb32c7ba53
I-Zafirov <ivoomail@gmail.com>
2022-07-27 17:30:56 +0300

    commit

---
+++ src/T09InterfacesAndAbstraction/exercise/E07CollectionHierarchy/Collection.java
@@ -0,0 +1,14 @@
+package T09InterfacesAndAbstraction.exercise.E07CollectionHierarchy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Collection {
+    protected int maxSize;
+    protected List<String> items;
+
+    public Collection() {
+        maxSize = 100;
+        items = new ArrayList<>();
+    }
+}
