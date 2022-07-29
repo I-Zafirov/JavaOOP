e3d6596fc45b255d3da2094cc6dccb5cc9123ae0
I-Zafirov <ivoomail@gmail.com>
2022-07-29 08:37:03 +0300

    commit

---
+++ src/T11Polymorphism/exercise/E01Vehicles/Messages.java
@@ -0,0 +1,6 @@
+package T11Polymorphism.exercise.E01Vehicles;
+
+public class Messages {
+    public static final String TRAVELING = "%s travelled %s km";
+    public static final String NOT_TRAVELING = "%s needs refueling";
+}
