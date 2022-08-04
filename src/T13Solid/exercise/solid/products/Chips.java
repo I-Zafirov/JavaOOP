202e56b3fbeeb9d0cc7bef64901e1b80f7025fb8
I-Zafirov <ivoomail@gmail.com>
2022-08-04 10:53:55 +0300

    commit

---
+++ src/T13Solid/exercise/solid/products/Chips.java
@@ -0,0 +1,16 @@
+package T13Solid.exercise.solid.products;
+
+public class Chips extends Food implements Product{
+    public static final double CALORIES_PER_100_GRAMS = 529.0;
+
+    public Chips(double grams) {
+        super(grams);
+    }
+
+    @Override
+    public double getAmountOfCalories() {
+        return (CALORIES_PER_100_GRAMS / 100) * this.getGrams();
+    }
+}
+
+
