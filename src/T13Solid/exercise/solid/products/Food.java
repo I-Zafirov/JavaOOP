44fc338a8c51f1e0126cbd57caf90df1b9ada89d
I-Zafirov <ivoomail@gmail.com>
2022-08-04 10:54:42 +0300

    commit

---
+++ src/T13Solid/exercise/solid/products/Food.java
@@ -0,0 +1,17 @@
+package T13Solid.exercise.solid.products;
+
+public abstract class Food {
+private double grams;
+
+public Food(double grams){
+    this.grams = grams;
+}
+
+public double getGrams(){
+    return grams;
+}
+
+    double getFoodAmountInKilograms(){
+    return 1000 / this.getGrams();
+}
+}
