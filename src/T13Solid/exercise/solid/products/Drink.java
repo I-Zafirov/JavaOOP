4e6c132f549978a1795fb1d44e7cdf0f2b41ad01
I-Zafirov <ivoomail@gmail.com>
2022-08-04 10:54:31 +0300

    commit

---
+++ src/T13Solid/exercise/solid/products/Drink.java
@@ -0,0 +1,23 @@
+package T13Solid.exercise.solid.products;
+
+public abstract class Drink {
+    private final double density;
+    private final double milliliters;
+
+    protected Drink(double density, double milliliters) {
+        this.density = density;
+        this.milliliters = milliliters;
+    }
+
+    public double getMilliliters(){
+        return this.milliliters;
+    }
+
+    public double getDensity(){
+        return this.density;
+    }
+
+    double getDrinkAmountInLiters(){
+        return  (1000 / this.getMilliliters()) * getDensity();
+    }
+}
