47d9936382df65f73d608cc93ec492e5e3b2bbc2
I-Zafirov <ivoomail@gmail.com>
2022-08-05 08:00:13 +0300

    commit

---
+++ src/T13Solid/exercise/solid/Printer.java
@@ -0,0 +1,10 @@
+package T13Solid.exercise.solid;
+
+import java.util.List;
+
+public class Printer {
+
+    public void print(String format, double amount){
+        System.out.printf(format, amount);
+    }
+}
