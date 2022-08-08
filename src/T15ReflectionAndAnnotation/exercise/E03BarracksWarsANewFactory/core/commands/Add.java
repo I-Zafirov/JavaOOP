75701f2eb1c4312ba75c0745b85867b071aff759
I-Zafirov <ivoomail@gmail.com>
2022-08-08 11:14:45 +0300

    commit

---
+++ src/T15ReflectionAndAnnotation/exercise/E03BarracksWarsANewFactory/core/commands/Add.java
@@ -0,0 +1,39 @@
+package T15ReflectionAndAnnotation.exercise.E03BarracksWarsANewFactory.core.commands;
+
+import T15ReflectionAndAnnotation.exercise.E03BarracksWarsANewFactory.annotations.Inject;
+import T15ReflectionAndAnnotation.exercise.E03BarracksWarsANewFactory.interfaces.Unit;
+import T15ReflectionAndAnnotation.exercise.E03BarracksWarsANewFactory.interfaces.Repository;
+import T15ReflectionAndAnnotation.exercise.E03BarracksWarsANewFactory.interfaces.UnitFactory;
+import jdk.jshell.spi.ExecutionControl;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Add extends Command {
+
+    @Inject
+    private Repository repository;
+
+    @Inject
+    private UnitFactory unitFactory;
+
+    public Add(String[] data) {
+        super(data);
+    }
+
+    @Override
+    public String execute() throws ClassNotFoundException,
+            NoSuchMethodException,
+            InstantiationException,
+            IllegalAccessException,
+            InvocationTargetException,
+            ExecutionControl.NotImplementedException {
+
+        final String unitType = getData()[1];
+
+        final Unit unitToAdd = this.unitFactory.createUnit(unitType);
+
+        this.repository.addUnit(unitToAdd);
+
+        return unitType + " added!";
+    }
+}
