119f2d44e3199807b015225aff46325d3b2cdb23
I-Zafirov <ivoomail@gmail.com>
2022-08-09 10:07:48 +0300

    commit

---
+++ src/T15ReflectionAndAnnotation/exercise/E03BarracksWarsANewFactory/interfaces/Destroyable.java
@@ -0,0 +1,8 @@
+package T15ReflectionAndAnnotation.exercise.E03BarracksWarsANewFactory.interfaces;
+
+public interface Destroyable {
+    
+    int getHealth();
+    
+    void setHealth(int health);
+}
