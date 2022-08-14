c64ba1fdaa5c629d405f0dab4c97aa849c16f6b5
I-Zafirov <ivoomail@gmail.com>
2022-08-14 20:56:18 +0300

    commit

---
+++ src/T15ReflectionAndAnnotation/exercise/E04BarracksWarsTheCommandsStrikeBack/interfaces/CommandFactory.java
@@ -0,0 +1,13 @@
+package T15ReflectionAndAnnotation.exercise.E04BarracksWarsTheCommandsStrikeBack.interfaces;
+
+import T15ReflectionAndAnnotation.exercise.E04BarracksWarsTheCommandsStrikeBack.core.commands.Command;
+
+import java.lang.reflect.InvocationTargetException;
+
+public interface CommandFactory {
+    Command createCommand(String commandName, String[] data) throws ClassNotFoundException,
+            NoSuchMethodException,
+            IllegalAccessException,
+            InvocationTargetException,
+            InstantiationException;
+}
