f5bfb6c53c6651452e14434be0ef27c7b844e707
I-Zafirov <ivoomail@gmail.com>
2022-08-14 20:56:41 +0300

    commit

---
+++ src/T15ReflectionAndAnnotation/exercise/E04BarracksWarsTheCommandsStrikeBack/interfaces/Destroyable.java
@@ -0,0 +1,8 @@
+package T15ReflectionAndAnnotation.exercise.E04BarracksWarsTheCommandsStrikeBack.interfaces;
+
+public interface Destroyable {
+    
+    int getHealth();
+    
+    void setHealth(int health);
+}
