075fbd78ea7d1b8d32776aea6a9f037a1c925176
I-Zafirov <ivoomail@gmail.com>
2022-08-17 10:54:04 +0300

    commit

---
+++ src/T15ReflectionAndAnnotation/exercise/E05BarracksWarsReturnOfTheDependencies/interfaces/Destroyable.java
@@ -0,0 +1,8 @@
+package T15ReflectionAndAnnotation.exercise.E05BarracksWarsReturnOfTheDependencies.interfaces;
+
+public interface Destroyable {
+    
+    int getHealth();
+    
+    void setHealth(int health);
+}
