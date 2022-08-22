2664a87cb266e2a06b002684e4d881f4ffa12647
I-Zafirov <ivoomail@gmail.com>
2022-08-22 12:01:22 +0300

    commit

---
+++ src/T18DebuggingTechniques/aquarium/repositories/interfaces/Repository.java
@@ -0,0 +1,12 @@
+package T18DebuggingTechniques.aquarium.repositories.interfaces;
+
+import aquarium.models.decorations.interfaces.Decoration;
+
+public interface Repository  {
+
+    void add(Decoration decoration);
+
+    boolean remove(Decoration decoration);
+
+    Decoration findByType(String type);
+}
